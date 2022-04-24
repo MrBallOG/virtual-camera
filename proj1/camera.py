fc7a2da4ca9afb77034498f1c1cc561a0d48d348
Hubert Piłka <hubert.pilka@outlook.com>
2022-04-24 17:32:05 +0200

    translation and rotation

--- proj1/camera.py
+++ proj1/camera.py
@@ -1,11 +1,21 @@
 import numpy as np
 
 
+T_VAL = 0.2
+T_POINTS = np.matrix(dtype=np.double, data=np.array([[T_VAL, 0, 0, 1],
+                                                     [-T_VAL, 0, 0, 1],
+                                                     [0, T_VAL, 0, 1],
+                                                     [0, -T_VAL, 0, 1],
+                                                     [0, 0, T_VAL, 1],
+                                                     [0, 0, -T_VAL, 1]])).T
+
+
 class Camera:
     def __init__(self):
-        self.point4 = np.mat(dtype=np.double, data=[0, 0, 0, 1]).T
+        self.point = np.mat(dtype=np.double, data=[0, 0, 0, 1]).T
         self.angle = np.mat(dtype=np.int16, data=[0, 0, 0]).T
         self.q = self._euler_to_quaternion()
+        self.q_con = self._quaternion_conjugate()
 
     def rotate_x_pos(self):
         self._rotate_pos(0)
@@ -25,35 +35,92 @@ class Camera:
     def rotate_z_neg(self):
         self._rotate_neg(2)
 
-    def _rotate_pos(self, i):
+    def _rotate_pos(self, i: int):
         current = self.angle[i, 0]
         self.angle[i, 0] = current + 5 if current < 355 else 0
         self.q = self._euler_to_quaternion()
+        self.q_con = self._quaternion_conjugate()
 
-    def _rotate_neg(self, i):
+    def _rotate_neg(self, i: int):
         current = self.angle[i, 0]
         self.angle[i, 0] = current - 5 if current > 0 else 355
         self.q = self._euler_to_quaternion()
+        self.q_con = self._quaternion_conjugate()
 
-    def _euler_to_quaternion(self):
-        phi = deg_to_rad(self.angle[0, 0])
-        theta = deg_to_rad(self.angle[1, 0])
-        psi = deg_to_rad(self.angle[2, 0])
+    def _euler_to_quaternion(self) -> np.matrix:
+        phi, theta, psi = deg_to_rad(self.angle)
 
-        cos_phi = np.cos(phi/2)
-        sin_phi = np.sin(phi/2)
-        cos_theta = np.cos(theta/2)
-        sin_theta = np.sin(theta/2)
-        cos_psi = np.cos(psi/2)
-        sin_psi = np.sin(psi/2)
+        cos_phi = np.cos(phi / 2)
+        sin_phi = np.sin(phi / 2)
+        cos_theta = np.cos(theta / 2)
+        sin_theta = np.sin(theta / 2)
+        cos_psi = np.cos(psi / 2)
+        sin_psi = np.sin(psi / 2)
 
         w = cos_phi * cos_theta * cos_psi + sin_phi * sin_theta * sin_psi
         x = sin_phi * cos_theta * cos_psi - cos_phi * sin_theta * sin_psi
         y = cos_phi * sin_theta * cos_psi + sin_phi * cos_theta * sin_psi
         z = cos_phi * cos_theta * sin_psi - sin_phi * sin_theta * cos_psi
 
-        return np.mat(dtype=np.double, data=[w, x, y, z]).T
+        return np.mat(dtype=np.double, data=[w[0, 0], x[0, 0], y[0, 0], z[0, 0]]).T
+
+    def _quaternion_conjugate(self) -> np.matrix:
+        return np.mat(dtype=np.double, data=[self.q[0, 0], -self.q[1, 0], -self.q[2, 0], -self.q[3, 0]]).T
+
+    def rotate_point(self, point: np.matrix) -> np.matrix:
+        point_at_origin = subtract_points(point, self.point)
+        rotated = self._rotate(point_at_origin)
+
+        return add_points(np.mat(dtype=np.double, data=[rotated[1, 0], rotated[2, 0], rotated[3, 0], 1]).T, self.point)
+
+    def _rotate(self, point: np.matrix) -> np.matrix:
+        q_p = np.mat(dtype=np.double, data=[
+                     0, point[0, 0], point[1, 0], point[2, 0]]).T
+        return hamilton_product(hamilton_product(self.q, q_p), self.q_con)
+
+    def translate_x_pos(self):
+        self._translate(0)
+
+    def translate_x_neg(self):
+        self._translate(1)
+
+    def translate_y_pos(self):
+        self._translate(2)
+
+    def translate_y_neg(self):
+        self._translate(3)
+
+    def translate_z_pos(self):
+        self._translate(4)
+
+    def translate_z_neg(self):
+        self._translate(5)
+
+    def _translate(self, i: int):
+        rotated = self._rotate(T_POINTS[:, i])
+        self.point = add_points(self.point, np.mat(dtype=np.double, data=[
+                                rotated[1, 0], rotated[2, 0], rotated[3, 0], 1]).T)
 
 
 def deg_to_rad(deg: int):
     return deg / 180 * np.pi
+
+
+def hamilton_product(q1: np.matrix, q2: np.matrix):
+    w1, x1, y1, z1 = q1
+    w2, x2, y2, z2 = q2
+
+    w = w1 * w2 - x1 * x2 - y1 * y2 - z1 * z2
+    x = w1 * x2 + x1 * w2 + y1 * z2 - z1 * y2
+    y = w1 * y2 + y1 * w2 + z1 * x2 - x1 * z2
+    z = w1 * z2 + z1 * w2 + x1 * y2 - y1 * x2
+
+    return np.mat(dtype=np.double, data=[w[0, 0], x[0, 0], y[0, 0], z[0, 0]]).T
+
+
+def add_points(p1: np.matrix, p2: np.matrix) -> np.matrix:
+    return np.mat(dtype=np.double, data=[p1[0, 0] + p2[0, 0], p1[1, 0] + p2[1, 0], p1[2, 0] + p2[2, 0], 1]).T
+
+
+def subtract_points(p1: np.matrix, p2: np.matrix, ) -> np.matrix:
+    return np.mat(dtype=np.double, data=[p1[0, 0] - p2[0, 0], p1[1, 0] - p2[1, 0], p1[2, 0] - p2[2, 0], 1]).T
--- proj1/main.py
+++ proj1/main.py
@@ -1,4 +1,4 @@
-from camera import Camera
+from camera import Camera, deg_to_rad, hamilton_product, T_POINTS
 import numpy as np
 
 
@@ -10,9 +10,36 @@ def main():
         cam.angle[i, 0] = l[i]
 
     q = cam._euler_to_quaternion()
+    cam.q = q
+    cam.q_con = cam._quaternion_conjugate()
     print(q)
 
-    print(cam.angle)
+    v = np.mat(dtype=np.double, data=[1, 0, 0, 1]).T
+    print(v)
+
+    cam.point[0, 0] = 3
+    cam.point[1, 0] = 5
+    cam.point[2, 0] = 15
+    v2 = cam.rotate_point(v)
+    print(cam.point)
+    cam.translate_x_pos()
+    print(np.round(v2, decimals=2))
+
+    print(cam.point)
+
+    # x, y, z = deg_to_rad(cam.angle)
+    # print(x[0, 0])
+    # x, y, z = cam.angle
+    # print(deg_to_rad(x))
+    # print(hamilton_product(cam.q, cam.q))
+
+    # should_render = True
+
+    # while True:
+    #     # key check
+    #     if should_render:
+    #         # do some
+    #         should_render = False
 
 
 main()
