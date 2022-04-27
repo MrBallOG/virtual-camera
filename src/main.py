85d975412456e02a95110efb7cd15432eecdd93a
Hubert Piłka <hubert.pilka@outlook.com>
2022-04-28 01:31:50 +0200

    enabling rotation and movement at the same time

--- src/main.py
+++ src/main.py
@@ -72,44 +72,37 @@ def main():
                     cam.zoom_in()
                 elif event.key == pg.K_x:
                     cam.zoom_out()
-                elif event.key == pg.K_v:
-                    cam.rotate_x_pos()
-                elif event.key == pg.K_b:
-                    cam.rotate_x_neg()
-                elif event.key == pg.K_n:
-                    cam.rotate_y_pos()
-                elif event.key == pg.K_m:
-                    cam.rotate_y_neg()
         keys = pg.key.get_pressed()
 
         if keys[pg.K_d] > 0:
             cam.translate_x_neg()
-        elif keys[pg.K_a] > 0:
+        if keys[pg.K_a] > 0:
             cam.translate_x_pos()
-        elif keys[pg.K_w] > 0:
+        if keys[pg.K_w] > 0:
             cam.translate_y_pos()
-        elif keys[pg.K_s] > 0:
+        if keys[pg.K_s] > 0:
             cam.translate_y_neg()
-        elif keys[pg.K_q] > 0:
+        if keys[pg.K_q] > 0:
             cam.translate_z_pos()
-        elif keys[pg.K_e] > 0:
+        if keys[pg.K_e] > 0:
             cam.translate_z_neg()
-        elif keys[pg.K_k] > 0:
+        if keys[pg.K_k] > 0:
             cam.rotate_x_neg()
-        elif keys[pg.K_i] > 0:
+        if keys[pg.K_i] > 0:
             cam.rotate_x_pos()
-        elif keys[pg.K_l] > 0:
+        if keys[pg.K_l] > 0:
             cam.rotate_y_pos()
-        elif keys[pg.K_j] > 0:
+        if keys[pg.K_j] > 0:
             cam.rotate_y_neg()
-        elif keys[pg.K_u] > 0:
+        if keys[pg.K_u] > 0:
             cam.rotate_z_pos()
-        elif keys[pg.K_o] > 0:
+        if keys[pg.K_o] > 0:
             cam.rotate_z_neg()
-        else:
-            if len(pg.event.get()) == 0 and not first_time:
-                first_time = False
+        if not first_time:
+            if len(pg.event.get()) == 0 and sum(_ for _ in keys) == 0:
                 continue
+        else:
+            first_time = False
 
         screen.fill(black)
 
